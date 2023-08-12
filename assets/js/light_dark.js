3ad0d8d2bf9f7823364c2d455752000ca15ad9c8
ianiiaannn <ianiiaannn@gmail.com>
2023-08-12 11:05:55 +0800

    utterances theme switch

--- assets/js/light_dark.js
+++ assets/js/light_dark.js
@@ -1,10 +1,23 @@
 const btn = document.querySelector(".btn-light-dark");
 const moon = document.querySelector(".moon");
 const sun = document.querySelector(".sun");
+const utterances = document.querySelector('.utterances-frame')
 
 const themeFromLS = localStorage.getItem("theme")
 const themeFromHugo = document.body.classList.contains("dark-theme") ? "dark" : null
 const currentTheme = themeFromLS ? themeFromLS : themeFromHugo;
+
+function utterancesTheme(theme) {
+  if (document.querySelector('.utterances-frame')) {
+    const message = {
+      type: 'set-theme',
+      theme: `github-${theme}`
+    };
+    const iframe = document.querySelector('.utterances-frame');
+    iframe.contentWindow.postMessage(message, '*');
+  }
+}
+
 
 if (currentTheme == "dark") {
   document.body.classList.add("dark-theme");
@@ -16,6 +29,7 @@ if (currentTheme == "dark") {
   sun.style.display = 'none';
 }
 
+
 btn.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
   let theme = "light";
@@ -24,9 +38,11 @@ btn.addEventListener("click", function () {
     theme = "dark";
     moon.style.display = 'none';
     sun.style.display = 'block';
+    utterancesTheme(theme);
   } else {
     moon.style.display = 'block';
     sun.style.display = 'none';
+    utterancesTheme(theme);
   }
   localStorage.setItem("theme", theme);
 });
